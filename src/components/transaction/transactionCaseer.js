59f0aed9e4ebbea7afe26781813fa59928dd22a6
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2019-09-03 18:51:21 +0700

    adding barcode

--- src/components/transaction/transactionCaseer.js
+++ src/components/transaction/transactionCaseer.js
@@ -5,16 +5,20 @@ import { Button, Text } from "react-native-elements";
 import { primaryColor, redColor } from "../../api/constant";
 import { resetTimer } from "../../redux/actions/time";
 import { connect } from "react-redux";
+import Barcode from "react-native-barcode-builder";
 
 class transactionCaseer extends Component {
   state = {
-    tableNumber: undefined
+    tableNumber: undefined,
+    transactionId: undefined
   };
 
   async componentDidMount() {
     const tableNumber = await AsyncStorage.getItem("@tableNumber");
+    const transactionId = await AsyncStorage.getItem("@transactionId");
     this.setState({
-      tableNumber
+      tableNumber,
+      transactionId: transactionId.toString()
     });
   }
 
@@ -34,24 +38,26 @@ class transactionCaseer extends Component {
               fontSize: 20,
               marginHorizontal: 20,
               textAlign: "center",
-              color: primaryColor
+              color: primaryColor,
+              marginBottom: 20
             }}
           >
-            PLEASE BRING THE IPAD TO THE CASHIER TO PROCEED WITH THE PAYMENT
+            PLEASE BRING THE PHONE TO THE CASHIER TO PROCEED WITH THE PAYMENT
           </Text>
-          <Text
-            style={{ marginTop: 20 }}
-            h1
-            h1Style={{ fontSize: 40, color: redColor }}
-          >
-            #{this.state.tableNumber}
-          </Text>
+          {this.state.transactionId ? (
+            <Barcode
+              value={this.state.transactionId}
+              format="CODE128"
+              width={2}
+              text={this.state.transactionId}
+            />
+          ) : null}
         </View>
         <Button
           title={"Back to Homepage"}
           onPress={this.goHome}
           buttonStyle={{ backgroundColor: primaryColor }}
-          containerStyle={{ marginHorizontal: 20, marginTop: 20 }}
+          containerStyle={{ marginHorizontal: 20, marginTop: 30 }}
         />
       </View>
     );
